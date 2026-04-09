c816bfbc4f08119e9064bbed40f70bab09982f6f
Sanzcloud-web <sanztheopro@gmail.com>
2026-04-09 15:13:57 +0200

    Fix gallery lightbox navigation bug (prev/next)
    prevImage used imagesCollection[index] instead of [index-1]
    nextImage used imagesCollection[index] instead of [index+1]

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -146,17 +146,15 @@
         });
       }
       let index = 0,
-        next = null;
+        prev = null;
 
       $(imagesCollection).each(function(i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
-          index = i ;
+          index = i;
         }
       });
-      next =
-        imagesCollection[index] ||
-        imagesCollection[imagesCollection.length - 1];
-      $(".lightboxImage").attr("src", $(next).attr("src"));
+      prev = imagesCollection[index - 1] || imagesCollection[imagesCollection.length - 1];
+      $(".lightboxImage").attr("src", $(prev).attr("src"));
     },
     nextImage() {
       let activeImage = null;
@@ -192,7 +190,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      next = imagesCollection[index + 1] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
